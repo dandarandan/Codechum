7efea55867852b2a9152b18a34da0fb5726a1c02
Dan <dandelprado@gmail.com>
2022-11-05 03:36:40 +0800

    How to Print - 4. Ai's Age

---
+++ aiAge.c
@@ -0,0 +1,12 @@
+/*
+Instruction:
+1. Manually calculate Ai's age by hand by subtracting the year of its invention, 1955, to the current year 2020.
+2. This time around, try writing the whole code on your own, including the basic C program structure to practice.
+*/
+
+#include <stdio.h>
+int main (void) {
+
+    printf("%d", 2020 - 1955);
+    return 0;
+}
