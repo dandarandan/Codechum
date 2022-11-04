089cc3bc57b3b0570aa62dafecc57f4e94f119ac
Dan <dandelprado@gmail.com>
2022-11-05 03:31:58 +0800

    How to Print - 3. Lyrics Guide

---
+++ lyricsGuide.c
@@ -0,0 +1,9 @@
+//Print out the children's song lyric "Twinkle \ twinkle \ little star". Make use of what you've learned from escape sequences to generate backslashes in your printf() function.
+
+#include<stdio.h>
+
+int main(void) {
+    printf("Twinkle \\ twinkle \\ little star");
+
+    return 0;
+    }
