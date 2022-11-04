644d6cb16c9dc9dc34bc4945abae0563ac0b7466
Dan <dandelprado@gmail.com>
2022-11-05 03:29:16 +0800

    How to Print - 2. Greetings with Feelings

---
+++ greetingsWithFeelings.c
@@ -0,0 +1,8 @@
+//You are asked to print out "Hello, World!" but with each word on a different line and with one line space separating the two words on the statement.
+
+#include <stdio.h>
+int main (void) {
+    printf("Hello, \n\nWorld!");
+
+    return 0;
+}
